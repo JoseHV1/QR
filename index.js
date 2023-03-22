6e253c25d2323d31dfc2e6604adaa9c2cc470d39
JoseHV1 <jitprogramadores@gmail.com>
2023-03-22 13:22:48 -0430

    importes fixes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(flash());
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './views'));
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ "extended": true }));
 
 app.use('/', routes);
