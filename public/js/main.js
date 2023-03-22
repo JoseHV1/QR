844d4b03fb2e9b1591f78ec20746072346403310
JoseHV1 <jitprogramadores@gmail.com>
2023-03-22 12:42:08 -0430

    interface design

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,43 @@
 window.onload = () => {
-    // alert('holas');
+    alert('holas');
     const container_reader = document.getElementById('container_reader');
+    const container_result = document.getElementById('container_result');
+    const result = document.getElementById('result');
 
-    var scanner = new Instascan.Scanner({
-        video: container_reader,
-        scanPeriod: 5,
-        mirror: false 
-    });
-
-    Instascan.Camera.getCameras().then(function(cameras) {
-        console.log(cameras);
-        if (cameras.length > 0) {
-            scanner.start(cameras[0])
-        } else {
-            console.log('No se han encontrado camaras');
-            alert('Camaras no encontradas');
-        }
-    }).catch(function(e) {
-        console.log(e);
-        alert(e);
-    });
-
-    scanner.addListener('scan', function(response) {
-        console.log("Contenido: "+response);
-    });
-
-    // var scanner = new Instascan.Scanner({ video: container_reader });
-    // scanner.addListener('scan', function (content, image) {
-    //     console.log(content);
+    // var scanner = new Instascan.Scanner({
+    //     video: container_reader,
+    //     scanPeriod: 5,
+    //     mirror: false 
     // });
 
-    // Instascan.Camera.getCameras().then(function (cameras) {
+    // Instascan.Camera.getCameras().then(function(cameras) {
+    //     console.log(cameras);
     //     if (cameras.length > 0) {
-    //         scanner.start(cameras[0]);
+    //         scanner.start(cameras[0])
+    //     } else {
+    //         console.log('No se han encontrado camaras');
+    //         alert('Camaras no encontradas');
     //     }
+    // }).catch(function(e) {
+    //     console.log(e);
+    //     alert(e);
     // });
+
+    // scanner.addListener('scan', function(response) {
+    //     console.log("Contenido: "+response);
+    // });
+
+    var scanner = new Instascan.Scanner({ video: container_reader });
+    scanner.addListener('scan', function (content, image) {
+        console.log(content);
+        console.log(image);
+        container_result.classList().remove('d_none');
+        result.textContent = content;
+    });
+
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        }
+    });
 }
